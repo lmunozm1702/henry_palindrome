acf77d52a6ad05f49200f1d9002ccc45e657066b
Luis Muñoz <l.munoz.m@outlook.com>
2025-03-17 15:44:50 -0300

    Update index.js to validate palindrome
    index.js
    - remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower)
    - Set and display the asked message for palindrome validation.
    - Moves void string validation from listener to palindrome function beacuse after removes non-alpahnumeric characters the string shpolud be void yet.
    
    README.md
    - Check all passed test for user histories.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const checkBtn = document.getElementById('check-btn');
 const textInput = document.getElementById('text-input');
+const result = document.getElementById('result');
 
 checkBtn.addEventListener('click', () => {
-  if (textInput.value === '') {
-    alert('Please input a value.');
-    return;
-  }
-  checkPalindrome();
+  checkPalindrome(textInput.value);
   textInput.value = '';
 });
 
 checkPalindrome = str => {
+  //Remove all non alphanumeric characters and set to lowercase
+  strNormalized = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+  if (strNormalized === '') {
+    result.innerHTML = 'Please input a value.';
+    return;
+  }
+
+  if (strNormalized === strNormalized.split('').reverse().join('')) {
+    result.innerHTML = `${str} is a palindrome!`;
+  }
+  else {
+    result.innerHTML = `${str} is not a palindrome.`;
+  }
 };
 
